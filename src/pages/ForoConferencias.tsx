d51d6a1d9146ac2e5deb3f3e52ac6046b649b006
Jonathan_Morales <jmoralesc54@miumg.edu.gt>
2025-10-15 07:34:28 -0600

    correcion en foroConferencia uso de import no utilizado

--- src/pages/ForoConferencias.tsx
+++ src/pages/ForoConferencias.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useAuth } from '../context/AuthContext';
+// import { useAuth } from '../context/AuthContext';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 
